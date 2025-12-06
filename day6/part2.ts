f89626c0a77cb8d9c665e14089d911f4ec7a430c
Bas Bijma <Bas@gutsgusto.com>
2025-12-06 11:33:34 +0100

    part one done

--- day6/part2.ts
+++ day6/part2.ts
@@ -1,16 +1,31 @@
 import { getInputFile } from '../globals'; // automates getting input.txt for corresponding day for every day 
 const input = getInputFile(import.meta.url).split('\n').map(line => line.split(' '));
 let result = 0;
+let operations: any[] = [];
 
-console.log(input)
 
-for (let i = 0; i < input.length; i++) {
-    for(let j = 0; j < input[i].length; j++) {
-        if (input[i][j] === '0') {
-            result += 1;
-        }
+for (let i = 0; i < input[0].length; i++) {
+    const numbers = []
+    for(let j = 0; j < input.length; j++) {
+        numbers.push(input[j][i])
     }
+    operations.push(numbers)    
 }
 
 
-console.log(result)
+// for (const operation of operations) {
+//     let operationResult = 0
+//     const operator = operation[operation.length - 1]
+//     for (let i = 0; i < operation.length - 1; i++) {
+//         if (operator === '+') {
+//             operationResult += parseInt(operation[i])
+//         }
+//         if (operator === '*') {
+//             if (operationResult === 0) operationResult = 1
+//             operationResult *= parseInt(operation[i])
+//         }
+//     }
+//     result += operationResult
+// }
+
+console.log(operations);
