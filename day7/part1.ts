a39d18097fed0682719899857198ca987c84cf0a
Bas Bijma <Bas@gutsgusto.com>
2025-12-07 11:20:00 +0100

    big bang day 7

---
+++ day7/part1.ts
@@ -0,0 +1,16 @@
+import { getInputFile } from '../globals'; // automates getting input.txt for corresponding day for every day 
+const input = getInputFile(import.meta.url).split('\n').map(line => line.split(' '));
+let result = 0;
+
+console.log(input)
+
+for (let i = 0; i < input.length; i++) {
+    for(let j = 0; j < input[i].length; j++) {
+        if (input[i][j] === '0') {
+            result += 1;
+        }
+    }
+}
+
+
+console.log(result)
