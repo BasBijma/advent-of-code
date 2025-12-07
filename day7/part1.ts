5214ee5fd42199e94804a390d2e653b9d6cbeb46
Bas Bijma <Bas@gutsgusto.com>
2025-12-07 11:24:23 +0100

    day template updated

--- day7/part1.ts
+++ day7/part1.ts
@@ -6,9 +6,8 @@ console.log(input)
 
 for (let i = 0; i < input.length; i++) {
     for(let j = 0; j < input[i].length; j++) {
-        if (input[i][j] === '0') {
-            result += 1;
-        }
+        
+        
     }
 }
 
